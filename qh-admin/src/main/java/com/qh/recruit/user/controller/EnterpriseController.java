66730d3475c17d498603209f638118f30e6e8401
90828 <908288050@qq.com>
2025-05-09 21:26:27 +0800

    准备开发求职端和企业端

---
+++ qh-admin/src/main/java/com/qh/recruit/user/controller/EnterpriseController.java
@@ -0,0 +1,7 @@
+package com.qh.recruit.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/etp")
+public class EnterpriseController {
+}
---
+++ qh-admin/src/main/java/com/qh/recruit/user/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.qh.recruit.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("test")
+    public void test(){
+        System.out.println("test");
+    }
+}
