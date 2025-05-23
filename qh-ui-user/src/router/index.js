e41a5c15d6878b5ae5e050ac6390a0178e212b6a
yanli <14636319+missing-becomes-a-breeze@user.noreply.gitee.com>
2025-05-23 11:56:14 +0800

    调用聊天组件

--- qh-ui-user/src/router/index.js
+++ qh-ui-user/src/router/index.js
@@ -43,7 +43,7 @@ const routes = [
   },
   {
     path: '/communicate',
-    component: () => import('../views/jobseeker/communicate')
+    component: () => import('../components/Chatbox/index')
   },
 ]
 
